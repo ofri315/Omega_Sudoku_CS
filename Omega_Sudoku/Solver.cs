7fe0d5446c10434329b1b7d5072b8454d8b1f183
main <ofribenel@gmail.com>
2026-01-17 22:44:38 +0200

    method NumberInRow created in class Solver

---
+++ Omega_Sudoku/Solver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omega_Sudoku
+{
+    internal class Solver
+    {
+        /// <summary>
+        /// הפעולה בודקת אם מספר נמצא בשורה 
+        /// </summary>
+        /// <param name="mat">מטריצה המייצגת סודוקו</param>
+        /// <param name="rowNumber">מספר שורה</param>
+        /// <param name="number">מספר לבדיקה אם נמצא בשורה</param>
+        /// <returns>אמת אם המספר נמצא בשורה, ושקר אחרת</returns>
+        public bool NumberInRow(int[,] mat, int rowNumber, int number)
+        {
+            for (int i = 0; i < mat.GetLength(1); i++)
+            {
+                if (mat[rowNumber, i] == number)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
