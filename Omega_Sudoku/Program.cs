5af351be39a6542e53cb2eec7213d0f2a692ae7f
main <ofribenel@gmail.com>
2026-01-20 20:06:44 +0200

    fields and constractor in solver class added

--- Omega_Sudoku/Program.cs
+++ Omega_Sudoku/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
 
 namespace Omega_Sudoku
 {
@@ -19,25 +21,26 @@ namespace Omega_Sudoku
         }
         static void Main(string[] args)
         {
-            ConvertSudoku convertor = new ConvertSudoku();
+            //ConvertSudoku convertor = new ConvertSudoku();
             
-            int[,] mat = convertor.ConvertStringToMatrix("800000020004010000000700040060090003050000700100020000090000500000030400070000006");
-            printMatrix(mat);
+            //int[,] mat = convertor.ConvertStringToMatrix("800000020004010000000700040060090003050000700100020000090000500000030400070000006");
+            //printMatrix(mat);
 
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
 
-            Solver.SolveSudoku(mat);
+            //Solver.SolveSudoku(mat);
 
-            stopwatch.Stop();
-            TimeSpan elapsedTime = stopwatch.Elapsed;
+            //stopwatch.Stop();
+            //TimeSpan elapsedTime = stopwatch.Elapsed;
             
-            Console.WriteLine();
-            printMatrix(mat);
-            Console.WriteLine(elapsedTime.TotalSeconds);
+            //Console.WriteLine();
+            //printMatrix(mat);
+            //Console.WriteLine(elapsedTime.TotalSeconds);
 
-
-
+            HashSet<int> set =new HashSet<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine(set.Contains(4));
+            //Dictionary<int,HashSet<int>> 
 
         }
     }
