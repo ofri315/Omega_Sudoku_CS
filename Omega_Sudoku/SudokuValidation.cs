361b64f7c84ef039a4854c9e3bcbef111c4fc1e1
main <ofribenel@gmail.com>
2026-01-30 13:07:35 +0200

    Add a function that validates the count of each number in each row

--- Omega_Sudoku/SudokuValidation.cs
+++ Omega_Sudoku/SudokuValidation.cs
@@ -17,7 +17,7 @@ namespace Omega_Sudoku
         /// The method checks whether the length of the string is a square number.
         /// </summary>
         /// <returns>True if the length of the string is a square number, false otherwise</returns>
-        public bool checkLength()
+        public bool CheckLength()
         {
             double n=Math.Sqrt(this.sudokuExpression.Length);
             return n - (int)n == 0;
@@ -27,9 +27,39 @@ namespace Omega_Sudoku
         /// The method checks whether the sudoku string contains only numbers
         /// </summary>
         /// <returns>True if the string contains only numbers, false otherwise</returns>
-        public bool checkOnlyNumbers()
+        public bool CheckOnlyNumbers()
         {
             return this.sudokuExpression.All(char.IsDigit);
         }
+
+        /// <summary>
+        /// The method checks that each row has each number only one time
+        /// </summary>
+        /// <param name="sudokuMat">sudoku matrix</param>
+        /// <returns>True if in any row each digit appears only once, false otherwise.</returns>
+        public bool CheckRows(int[,] sudokuMat)
+        {
+            for (int i = 0; i < sudokuMat.GetLength(0); i++)
+            {
+                int[] countDigArr = new int[sudokuMat.GetLength(0)+1];
+                for (int j = 0;  j < countDigArr.Length;  j++)
+                {
+                    countDigArr[j] = 0;
+                }
+                for (int j = 0; j < sudokuMat.GetLength(1); j++)
+                {
+                    countDigArr[sudokuMat[i, j]]++;
+                }
+                for (int j = 1; j < countDigArr.Length; j++)
+                {
+                    Console.WriteLine(j);
+                    if (countDigArr[j] >1)
+                        return false;
+                }
+            }
+            return true;
+
+        }
+
     }
 }
