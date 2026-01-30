050f1dcd1b219319913b3343ef546fe6d27ec1a9
main <ofribenel@gmail.com>
2026-01-30 11:47:51 +0200

    Add a function that checks the length of the string

---
+++ Omega_Sudoku/SudokuValidation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omega_Sudoku
+{
+    internal class SudokuValidation
+    {
+        private string sudokuExpression;
+        public SudokuValidation(string sudokuExpression)
+        {
+            this.sudokuExpression = sudokuExpression;
+        }
+        /// <summary>
+        /// The method checks whether the length of the string is a square number.
+        /// </summary>
+        /// <returns>True if the length of the string is a square number, false otherwise</returns>
+        public bool checkLength()
+        {
+            double n=Math.Sqrt(this.sudokuExpression.Length);
+            return n - (int)n == 0;
+        }
+
+        
+
+
+    }
+}
