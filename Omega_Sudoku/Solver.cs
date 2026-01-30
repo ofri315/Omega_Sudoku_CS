eef8073219388a5abe2116fd98dd8eefc538fa88
main <ofribenel@gmail.com>
2026-01-30 18:19:45 +0200

    Fix Solver constractor, and InitDictRow, InitDictCol, and InitDictBlock functions

--- Omega_Sudoku/Solver.cs
+++ Omega_Sudoku/Solver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -10,6 +11,84 @@ namespace Omega_Sudoku
 {
     internal class Solver
     {
+        /// <summary>
+        /// struct המייצג תא בסודוקו
+        /// </summary>
+        public struct Position
+        {
+            public int row;
+            public int col;
+        }
+
+
+        private int[,] mat;
+        private Dictionary<int, HashSet<int>> dictRow=new Dictionary<int, HashSet<int>>();
+        private Dictionary<int, HashSet<int>> dictCol=new Dictionary<int, HashSet<int>>();
+        private Dictionary<Position, HashSet<int>> dictBlock=new Dictionary<Position, HashSet<int>>();
+
+        //Constractor
+        public Solver(int[,] mat, int start, int end)
+        {
+            this.mat = mat;
+            int n = mat.GetLength(0);
+            for (int i = start; i <= end; i++)
+            {
+                dictRow.Add(i, new HashSet<int>());
+            }
+            for (int i = start; i <= end; i++)
+            {
+                dictCol.Add(i, new HashSet<int>());
+            }
+            for (int i=0;i< Math.Sqrt(mat.GetLength(0));i+=3)
+            {
+                for (int j = 0; j < Math.Sqrt(mat.GetLength(1)); j+=3)
+                {
+                    Position p = new Position();
+                    p.row = i;
+                    p.col = j;
+                    dictBlock.Add(p,new HashSet<int>());
+                }
+            }
+        }
+
+        public void initDictRow()
+        {
+            for (int i = 0; i< mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if (mat[i,j]!=0)
+                        this.dictRow[i].Add(this.mat[i,j]);
+                }
+            }   
+        }
+        public void initDictCol()
+        {
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if (mat[i, j] != 0)
+                        this.dictCol[j].Add(this.mat[i, j]);
+                }
+            }
+        }
+        public void initDictBlock()
+        {
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if (mat[i, j] != 0)
+                    {
+                        Position p = new Position();
+                        p.row = (int)(i / 3);
+                        p.col = (int)(j / 3);
+                        this.dictBlock[p].Add(this.mat[i, j]);
+                    }
+                }
+            }
+        }
         /// <summary>
         /// הפעולה בודקת אם מספר נמצא בשורה 
         /// </summary>
@@ -72,15 +151,15 @@ namespace Omega_Sudoku
             }
             return false;
         }
-
-        /// <summary>
-        /// struct המייצג תא בסודוקו
-        /// </summary>
-        public struct Position
+         
+        public bool isValidPosition(int number, int row, int col)
         {
-            public int row;
-            public int col;
+            Position p = new Position();
+            p.row= (int)(row / 3) * 3;
+            p.col = (int)(col / 3) * 3;
+            return !this.dictRow[row].Contains(number) && !this.dictCol[col].Contains(number) && !this.dictBlock[p].Contains(number);
         }
+
 
 
         /// <summary>
@@ -88,12 +167,12 @@ namespace Omega_Sudoku
         /// </summary>
         /// <param name="mat">מטריצה המייצגת סודוקו</param>
         /// <returns>מחזירה struct של position המייצגת שורה ועמודה אם קיים תא ריק במטריצה, אחרת מחזירה null</returns>
-        public static Position? FindEmptyCell(int[,] mat)
+        public Position? FindEmptyCell()
         {
 
-            for (int i = 0; i < mat.GetLength(0); i++)
+            for (int i = 0; i < this.mat.GetLength(0); i++)
             {
-                for (int j = 0; j < mat.GetLength(1); j++)
+                for (int j = 0; j < this.mat.GetLength(1); j++)
                 {
                     if (mat[i,j]==0)
                     {
@@ -112,21 +191,24 @@ namespace Omega_Sudoku
         /// </summary>
         /// <param name="mat">מטריצה המייצגת סודוקו</param>
         /// <returns>אמת אם קיים פתרון תקין לסודוקו, שקר אחרת</returns>
-        public static bool SolveSudoku(int[,] mat)
+        public bool SolveSudokuRec()
         {
-            if (FindEmptyCell(mat)==null) //תנאי עצירה
+            if (FindEmptyCell()==null) //תנאי עצירה
             {
                 return true;
             }
-            Position pos = (Position)FindEmptyCell(mat);
+            Position pos = (Position)FindEmptyCell();
             int row=pos.row;
             int col = pos.col;
             for (int number = 1; number < 10; number++)
             {
-                if (!NumberInBlock(mat, row, col, number) && !NumberInRow(mat,row, number) && !NumberInCol(mat,col, number))
+                if (isValidPosition(number,row, col))
                 {
                     mat[row, col] = number;
-                    if (SolveSudoku(mat))
+                    this.dictRow[row].Add( number);
+                    this.dictCol[col].Add(number);
+                    this.dictBlock[pos].Add(number);
+                    if (SolveSudokuRec())
                     {
                         return true;
                     }
@@ -134,6 +216,9 @@ namespace Omega_Sudoku
                     {
                         mat[row, col] = 0; //אם אף אחד מהמספרים 1-9 לא מתאים,
                                            //זה אומר שיש בעיה באחד המספרים הקודמים שמוקמו
+                        this.dictRow[row].Remove(number);
+                        this.dictCol[col].Remove(number);
+                        this.dictBlock[pos].Remove(number);
                     }
                 }
                 
@@ -141,6 +226,8 @@ namespace Omega_Sudoku
             return false;
 
         }
+        
+
  
 
     }
