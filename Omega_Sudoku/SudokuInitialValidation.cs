45e7c11fd937149820dca86692a1314a91a7a1d4
main <ofribenel@gmail.com>
2026-02-06 18:14:18 +0200

    Add CheckSubMatrixLength function in SudokuInitialValidation

--- Omega_Sudoku/SudokuInitialValidation.cs
+++ Omega_Sudoku/SudokuInitialValidation.cs
@@ -13,10 +13,12 @@ namespace Omega_Sudoku
         {
             this.sudokuExpression = sudokuExpression;   
         }
+        
         /// <summary>
-        /// The method checks whether the length of the string is a square number.
+        /// The function checks whether the length of the string is a square number.
         /// </summary>
-        /// <returns>True if the length of the string is a square number, false otherwise</returns>
+        /// <returns>True if the length of the sudoku string is a square number</returns>
+        /// <exception cref="ArgumentException">if the length of the string is not a square number, the function throws ArgumentException</exception>
         public bool CheckSquareLength()
         {
             double n = Math.Sqrt(this.sudokuExpression.Length);
@@ -25,9 +27,26 @@ namespace Omega_Sudoku
             throw new ArgumentException("The length of the Sudoku string is not a square root.");
         }
 
+        /// <summary>
+        /// The function checks whether the length of the sudoku string is smaller then 81 (a larger number will not allow the string to be converted to a matrix so that each character is an cell in the matrix).
+        /// </summary>
+        /// <returns>true if the length of the sudoku string is smaller then 81 </returns>
+        /// <exception cref="ArgumentException">if the length of the string is larger then 81, the function throws ArgumentException</exception>
+        public bool CheckExpressionLength()
+        {
+            if (this.sudokuExpression.Length<=81)
+                return true;
+            throw new ArgumentException("The length is bigger then 81");
+        }
 
-
-
+        public bool CheckSubMatrixLength()
+        {
+            double rowColBlockLength= Math.Sqrt(this.sudokuExpression.Length);
+            double subMatrixLength= Math.Sqrt(rowColBlockLength);
+            if (subMatrixLength - (int)subMatrixLength == 0)
+                return true;
+            throw new ArgumentException("The length of the sub matrices sides is not equal (NxN)");
+        }
         /// <summary>
         /// The method checks whether the sudoku string contains only numbers
         /// </summary>
